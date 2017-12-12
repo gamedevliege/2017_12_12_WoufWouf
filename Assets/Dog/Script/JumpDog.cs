ed170671655f29a5d90b8d06e082dbe80da675be
Tristan Jardon <tristan.jardon@gmail.com>
2017-12-12 17:19:19 +0100

    It works

---
+++ Assets/Dog/Script/JumpDog.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDog : Dog {
+    
+    #region Public Members
+
+    public Rigidbody m_rigidbody;
+    public float m_thrust;
+
+    public AudioSource m_audioSource;
+
+    #endregion
+
+    #region Public void
+
+    public override void Bark(float volume, float frequence)
+    {
+        Debug.Log(volume);
+    }
+
+    public override void Jump(float heighCm)
+    {
+        m_rigidbody.AddForce(Vector3.up * heighCm * m_thrust, ForceMode.Impulse);
+    }
+
+    #endregion
+
+    #region System
+
+    void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+    #region Private an Protected Members
+
+    #endregion
+}
