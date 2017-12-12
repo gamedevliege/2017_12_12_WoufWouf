f171fef05e8b13cda1d62101b6bf2bb7c20e8646
jamscenter <jamscenter@gmail.com>
2017-12-12 15:54:09 +0100

    JumpingDog

---
+++ Assets/Dog/Script/Exemple/Team/Eloi/JumpingDog.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingDog : Dog {
+
+    public Rigidbody _affectedRigidbody;
+    public float _forceByCm;
+
+    public AudioSource _audioSource;
+    
+
+    public override void Bark(float volume, float frequence)
+    {
+        _audioSource.volume = volume;
+        _audioSource.Play();
+    }
+
+    public override void Jump(float heighCm)
+    {
+        _affectedRigidbody.AddForce(Vector3.up * (_forceByCm * heighCm), ForceMode.Impulse);
+    }
+    #region Public Members
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    void Start ()
+    {
+        
+    }
+
+    void Update ()
+    {
+        	
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
